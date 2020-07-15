6aa47406f51612b083bd1a003e8e7d0e549cf027
Jorge Ossa <jorgehossa@gmail.com>
2020-07-15 00:12:49 -0500

    Add react-router-dom and new components

---
+++ src/routes/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from '../containers/Home';
+import Login from '../containers/Login';
+import Register from '../containers/Register';
+import NotFound from '../containers/NotFound';
+
+const App = () => (
+  <Router>
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/login' component={Login} />
+      <Route exact path='/register' component={Register} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+);
+
+export default App;
+
