534d1320a7927e9e5c988ec84b4d19c1740fc73c
Jorge Ossa <jorgehossa@gmail.com>
2020-07-15 23:41:20 -0500

    Add remove action

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
-import { setFavorite } from '../actions';
+import { setFavorite, removeFavorite } from '../actions';
 import '../assets/styles/components/CarouselItem.scss';
 import playIcon from '../assets/static/play-icon.png';
 import plusIcon from '../assets/static/plus-icon.png';
+import removeIcon from '../assets/static/remove-icon.png';
 
 const CarouselItem = (props) => {
-  const { cover, title, year, contentRating, duration } = props;
+  const { id, cover, title, year, contentRating, duration } = props;
+
   const handleSetFavorite = () => {
     props.setFavorite({
-      cover, title, year, contentRating, duration,
+      id, cover, title, year, contentRating, duration,
     });
   };
+
+  const handleRemoveFavorite = itemId => props.removeFavorite(itemId);
+
   return (
     <div className='carousel-item'>
       <img className='carousel-item__img' src={cover} alt={title} />
@@ -20,6 +25,7 @@ const CarouselItem = (props) => {
         <div>
           <img className='carousel-item__details--img' src={playIcon} alt='Play Icon' />
           <img className='carousel-item__details--img' src={plusIcon} alt='Plus Icon' onClick={handleSetFavorite} />
+          <img className='carousel-item__details--img' src={removeIcon} alt='Remove Icon' onClick={() => handleRemoveFavorite(id)} />
         </div>
         <p className='carousel-item__details--title'>{title}</p>
         <p className='carousel-item__details--subtitle'>
@@ -40,6 +46,7 @@ CarouselItem.propTypes = {
 
 const mapDispatchToProps = {
   setFavorite,
+  removeFavorite,
 };
 
 export default connect(null, mapDispatchToProps)(CarouselItem);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,16 @@
+import { SET_FAVORITE, REMOVE_FAVORITE } from '../types';
+
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'SET_FAVORITE':
+    case SET_FAVORITE:
       return {
         ...state,
         myList: [...state.myList, action.payload],
+      };
+    case REMOVE_FAVORITE:
+      return {
+        ...state,
+        myList: state.myList.filter(items => items.id !== action.payload),
       };
     default:
       return state;
